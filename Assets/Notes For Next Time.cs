56928058817074e3ef13a714b1c1eaaade1c59e5
CodeNinjasCoquitlam <coquitlamninja@gmail.com>
2024-09-09 18:02:32 -0700

    Sept9

---
+++ Assets/Notes For Next Time.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesForNextTime : MonoBehaviour
+{
+    // Redo Score Ssystem
+    //Step 62 in workbook
+    //Make rocket animate properly
+    //Make SplashScreen Show Up Properly
+    //Notes
+
+}
--- Assets/Scripts/GameManagerScrpt.cs
+++ Assets/Scripts/GameManagerScrpt.cs
@@ -1,28 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class GameManagerScrpt : MonoBehaviour
 {
     private Spawner spawner;
     public GameObject title;
-    // Start is called before the first frame update
+    private Vector2 screenBounds;
+    public int FallDistance;
+    public int score;
+    public GameObject splash;
+    private Text _text;
+    public GameObject playerPrefab;
+    private GameObject player;
+    private bool gameStarted = false;
+
+
+
+    void ResetGame()
+    {
+        spawner.active = true;
+        title.SetActive(false);
+        player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
+        gameStarted = true;
+        splash.SetActive(false);
+    }
     void Start()
     {
         spawner.active = false;
         title.SetActive(true);
+        score = 0;
+        splash.SetActive(false);
+        
     }
     void Awake()
     {
+        _text = GameObject.Find("ScoreText").GetComponent<Text>();
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        player = playerPrefab;
+       
     }
-    // Update is called once per frame
+   void OnPlayerKilled()
+    {
+        spawner.active = false;
+        gameStarted = false;
+
+         splash.SetActive(true);
+    }
     void Update()
     {
-        if (Input.anyKeyDown)
+        _text.text = "Score: " + score;
+
+
+        if (!gameStarted)
         {
-            spawner.active = true;
-            title.SetActive(false);
+            if (Input.anyKeyDown)
+            {
+                ResetGame();
+            }
+        } else
+        {
+            if (!player)
+            {
+                OnPlayerKilled();
+            }
+        }
+        var nextBomb = GameObject.FindGameObjectsWithTag("bomb");
+
+        foreach (GameObject bombObject in nextBomb)
+        {
+            if (bombObject.transform.position.y < (-screenBounds.y) - FallDistance || !gameStarted)
+            {
+                score++;
+                Destroy(bombObject);
+                
+            }
         }
     }
 }
